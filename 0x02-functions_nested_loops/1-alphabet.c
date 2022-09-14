878cb419e38a0efd78bd3996ace408391758cb81
murylearn <murytarlah.learn@gmail.com>
2022-09-14 01:24:21 -0700

    finished task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry Point
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+	}
+
+	return (0);
+}
