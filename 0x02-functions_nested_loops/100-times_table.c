da5d8a2f79e5d726639d5288a792790f3b1d06a4
murytarlah <dknight.jim@gmail.com>
2022-09-14 13:17:05 +0100

    corrected task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -35,10 +35,17 @@ void print_times_table(int n)
 				_putchar(' ');
 				_putchar('0' + rep);
 			}
+			else if (rep < 100)
+			{
+				_putchar(' ');
+				_putchar('0' + rep / 10);
+				_putchar('0' + rep % 10);
+			}
 			else
 			{
-				_putchar('0' + rep);
-				_putchar('0' + rep);
+				_putchar('0' + rep / 100);
+				_putchar('0' + (rep - 100) / 10);
+				_putchar('0' + rep % 10);
 			}
 
 			if (b < n)
