9603b99bccef6bfea471f339a5a90081b7d1611f
murylearn <murytarlah.learn@gmail.com>
2022-09-14 02:33:43 -0700

    finished with task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry Point
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		print_alphabet();
+		i++;
+	}
+}
