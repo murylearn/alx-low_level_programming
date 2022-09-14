d4004c18cc606c116ac658762ccc2ac39ba185c8
murylearn <murytarlah.learn@gmail.com>
2022-09-14 04:21:53 -0700

    finished task 7 and corrected betty style for 6

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - printd the last digit of a number
+ * @n: the number we are looking for it last digits
+ * Return: 0
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
