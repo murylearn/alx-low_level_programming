85c44b270f309e9a8ecc7f054b82adedd5ad9af2
murylearn <murytarlah.learn@gmail.com>
2022-09-14 04:48:26 -0700

    finished task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - returns the sum of its parameters
+ * @a: param1 type int
+ * @b: param2 type int
+ * Return: 0
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
