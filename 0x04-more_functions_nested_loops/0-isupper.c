12b3e35b95824ba95bb8f40d628448fc04273cbb
murylearn <murytarlah.learn@gmail.com>
2022-09-15 05:47:52 -0700

    finished task 0 and updated READMEs

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to be checked
+ * Return: 0
+ */
+
+int _isupper(int c)
+{
+	return ((c >= 65 && c <= 90));
+}
