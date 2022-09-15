d22412d2fbe5b8b5bdc89acd008ee580726638d2
murylearn <murytarlah.learn@gmail.com>
2022-09-15 10:39:54 -0700

    finished task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line to the terminal
+ * @n: length of line
+ * Return: none
+ */
+
+void print_line(int n)
+{
+	int l = 0;
+
+	while (l < n && n > 0)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
